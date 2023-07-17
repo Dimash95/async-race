369cb63e0951e05f2aeac018b9793305609c0168
Dimash <dinmukhamed.amirov@gmail.com>
2023-07-17 18:59:39 +0600

    feat: add addEventListener create button

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './global.css';
+import redFlagImg from './asset/resource/svg/red-flag.png';
 import garageWinners from './components/garage-winners/garage-winners';
 garageWinners();
 
@@ -84,57 +85,64 @@ pageNumber.textContent = 'Page #1';
 garageContent.append(pageNumber);
 
 //////// * generate car
+// let i = 1;
 
-const selectRemoveCarName = document.createElement('div');
-selectRemoveCarName.classList.add('select-remove-car-name');
-garageContent.append(selectRemoveCarName);
+createButton.addEventListener('click', () => {
+  const carFullContainer = document.createElement('div');
+  carFullContainer.classList.add('car-full-container');
+  garageContent.append(carFullContainer);
 
-const select = document.createElement('button');
-select.textContent = 'Select';
-selectRemoveCarName.append(select);
+  const selectRemoveCarName = document.createElement('div');
+  selectRemoveCarName.classList.add('select-remove-car-name');
+  carFullContainer.append(selectRemoveCarName);
 
-const remove = document.createElement('button');
-remove.textContent = 'Remove';
-selectRemoveCarName.append(remove);
+  const select = document.createElement('button');
+  select.textContent = 'Select';
+  selectRemoveCarName.append(select);
 
-const carName = document.createElement('div');
-carName.textContent = 'Tesla';
-selectRemoveCarName.append(carName);
+  const remove = document.createElement('button');
+  remove.textContent = 'Remove';
+  selectRemoveCarName.append(remove);
 
-const roadContainer = document.createElement('div');
-roadContainer.classList.add('road-container');
-garageContent.append(roadContainer);
+  const carName = document.createElement('div');
+  carName.textContent = inputTextCreate.value;
+  inputTextCreate.value = '';
+  selectRemoveCarName.append(carName);
 
-const roadButtons = document.createElement('div');
-roadButtons.classList.add('road-buttons');
-roadContainer.append(roadButtons);
+  const roadContainer = document.createElement('div');
+  roadContainer.classList.add('road-container');
+  carFullContainer.append(roadContainer);
 
-const pointAButton = document.createElement('button');
-pointAButton.textContent = 'A';
-roadButtons.append(pointAButton);
+  const roadButtons = document.createElement('div');
+  roadButtons.classList.add('road-buttons');
+  roadContainer.append(roadButtons);
 
-const pointBButton = document.createElement('button');
-pointBButton.textContent = 'B';
-roadButtons.append(pointBButton);
+  const pointAButton = document.createElement('button');
+  pointAButton.textContent = 'A';
+  roadButtons.append(pointAButton);
 
-const fullRoad = document.createElement('div');
-fullRoad.classList.add('full-road');
-roadContainer.append(fullRoad);
+  const pointBButton = document.createElement('button');
+  pointBButton.textContent = 'B';
+  roadButtons.append(pointBButton);
 
-const road = document.createElement('div');
-road.classList.add('road');
-fullRoad.append(road);
+  const fullRoad = document.createElement('div');
+  fullRoad.classList.add('full-road');
+  roadContainer.append(fullRoad);
 
-const svg = `<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 640 512"><style>svg{fill:green}</style><path d="M171.3 96H224v96H111.3l30.4-75.9C146.5 104 158.2 96 171.3 96zM272 192V96h81.2c9.7 0 18.9 4.4 25 12l67.2 84H272zm256.2 1L428.2 68c-18.2-22.8-45.8-36-75-36H171.3c-39.3 0-74.6 23.9-89.1 60.3L40.6 196.4C16.8 205.8 0 228.9 0 256V368c0 17.7 14.3 32 32 32H65.3c7.6 45.4 47.1 80 94.7 80s87.1-34.6 94.7-80H385.3c7.6 45.4 47.1 80 94.7 80s87.1-34.6 94.7-80H608c17.7 0 32-14.3 32-32V320c0-65.2-48.8-119-111.8-127zM434.7 368a48 48 0 1 1 90.5 32 48 48 0 1 1 -90.5-32zM160 336a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>`;
+  const road = document.createElement('div');
+  road.classList.add('road');
+  fullRoad.append(road);
 
-const car = document.createElement('div');
-car.classList.add('car');
-car.innerHTML = svg;
-car.style.fill = 'red';
-road.append(car);
+  const svg = `<svg xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 640 512"><style>svg{fill:${inputColorCreate.value}}</style><path d="M171.3 96H224v96H111.3l30.4-75.9C146.5 104 158.2 96 171.3 96zM272 192V96h81.2c9.7 0 18.9 4.4 25 12l67.2 84H272zm256.2 1L428.2 68c-18.2-22.8-45.8-36-75-36H171.3c-39.3 0-74.6 23.9-89.1 60.3L40.6 196.4C16.8 205.8 0 228.9 0 256V368c0 17.7 14.3 32 32 32H65.3c7.6 45.4 47.1 80 94.7 80s87.1-34.6 94.7-80H385.3c7.6 45.4 47.1 80 94.7 80s87.1-34.6 94.7-80H608c17.7 0 32-14.3 32-32V320c0-65.2-48.8-119-111.8-127zM434.7 368a48 48 0 1 1 90.5 32 48 48 0 1 1 -90.5-32zM160 336a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>`;
 
-const redFlag = document.createElement('img');
-import redFlagImg from './asset/resource/svg/red-flag.png';
-redFlag.src = redFlagImg;
-redFlag.classList.add('red-flag');
-road.append(redFlag);
+  const car = document.createElement('div');
+  car.classList.add('car');
+  car.innerHTML = svg;
+  car.style.fill = 'red';
+  road.append(car);
+
+  const redFlag = document.createElement('img');
+  redFlag.src = redFlagImg;
+  redFlag.classList.add('red-flag');
+  road.append(redFlag);
+});

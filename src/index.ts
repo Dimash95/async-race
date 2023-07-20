0dbd7316d7d6d80e97bb72b51e1a05e9d807b07c
Dimash <dinmukhamed.amirov@gmail.com>
2023-07-20 21:41:02 +0600

    feat: add pagination

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import './global.css';
 import redFlagImg from './asset/resource/svg/red-flag.png';
 import garageWinners from './components/garage-winners/garage-winners';
 garageWinners();
+import { showSvg } from './car-svg';
+showSvg();
 
 //////// * mainButtons
 
@@ -90,14 +92,40 @@ const pageNumber = document.createElement('p');
 pageNumber.textContent = 'Page #1';
 garageContent.append(pageNumber);
 
+const allCars = document.createElement('div');
+allCars.classList.add('all-cars');
+garageContent.append(allCars);
+
 //////// * generate car
 let id = 0;
+
+// const carFullContainerContent = `<div class="select-remove-car-name">
+//     <button>Select</button>
+//     <button class="remove-button">Remove</button>
+//     <div id="car-name-${id}" class="car-name"></div>
+//   </div>
+//   <div class="road-container">
+//     <div class="road-buttons">
+//       <button>A</button>
+//       <button>B</button>
+//     </div>
+//     <div class="full-road">
+//       <div class="road">
+//         <div class="car"></div>
+//         <img class="red-flag" src="${redFlagImg}"/>
+//       </div>
+//     </div>
+//   </div>`;
+// const carFullContainer = document.createElement('div');
+// carFullContainer.innerHTML = carFullContainerContent;
+// allCars.append(carFullContainer);
+console.log(allCars);
 
 function createCar() {
   const carFullContainer = document.createElement('div');
   carFullContainer.setAttribute('id', `${id}`);
   carFullContainer.classList.add('car-full-container');
-  garageContent.append(carFullContainer);
+  allCars.append(carFullContainer);
 
   const selectRemoveCarName = document.createElement('div');
   selectRemoveCarName.classList.add('select-remove-car-name');
@@ -143,11 +171,9 @@ function createCar() {
   road.classList.add('road');
   fullRoad.append(road);
 
-  const svg = `<svg id="red-flag-${id}" xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 640 512"><style>svg{fill:black}</style><path d="M171.3 96H224v96H111.3l30.4-75.9C146.5 104 158.2 96 171.3 96zM272 192V96h81.2c9.7 0 18.9 4.4 25 12l67.2 84H272zm256.2 1L428.2 68c-18.2-22.8-45.8-36-75-36H171.3c-39.3 0-74.6 23.9-89.1 60.3L40.6 196.4C16.8 205.8 0 228.9 0 256V368c0 17.7 14.3 32 32 32H65.3c7.6 45.4 47.1 80 94.7 80s87.1-34.6 94.7-80H385.3c7.6 45.4 47.1 80 94.7 80s87.1-34.6 94.7-80H608c17.7 0 32-14.3 32-32V320c0-65.2-48.8-119-111.8-127zM434.7 368a48 48 0 1 1 90.5 32 48 48 0 1 1 -90.5-32zM160 336a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>`;
-
   const car = document.createElement('div');
   car.classList.add('car');
-  car.innerHTML = svg;
+  car.innerHTML = showSvg();
   car.style.fill = inputColorCreate.value;
   road.append(car);
 
@@ -158,6 +184,9 @@ function createCar() {
 
   removeContainer(document.querySelectorAll('.remove-button'), document.querySelectorAll('.car-full-container'));
   showQuantityCars();
+  displayPage();
+  displayPageNumbers();
+  showPage();
 }
 
 createButton.addEventListener('click', createCar);
@@ -173,6 +202,65 @@ function removeContainer(removeButtons: NodeListOf<Element>, carFullContainer: N
 
 function showQuantityCars() {
   const allCars = document.querySelectorAll('.car-full-container');
-  console.log(allCars.length);
   garageTitle.textContent = `Garage (${allCars.length})`;
+}
+
+const paginationContainer = document.createElement('div');
+paginationContainer.classList.add('pagination-container');
+garageContent.append(paginationContainer);
+
+const pagination = document.createElement('div');
+pagination.classList.add('pagination');
+paginationContainer.append(pagination);
+
+const pageNumbers = document.createElement('ul');
+pagination.append(pageNumbers);
+
+const CAR_ROWS = 7;
+let currentPage = 1;
+
+////////////// * pagination
+
+function displayPage() {
+  const start = currentPage * CAR_ROWS - CAR_ROWS;
+  const end = start + CAR_ROWS;
+  const paginatedData = [...allCars.children].slice(start, end);
+
+  [...allCars.children].forEach((e) => {
+    e.classList.add('disabled');
+  });
+
+  paginatedData.forEach((e) => {
+    e.classList.remove('disabled');
+  });
+}
+
+function displayPageNumbers() {
+  const pages = Math.ceil(allCars.children.length / CAR_ROWS);
+
+  const liAll = document.querySelectorAll('.li');
+
+  liAll.forEach((e) => {
+    pageNumbers.removeChild(e);
+  });
+
+  for (let i = 1; i <= pages; i++) {
+    const li = document.createElement('li');
+    li.classList.add('li');
+    li.innerHTML = i.toString();
+    pageNumbers.appendChild(li);
+  }
+}
+
+function showPage() {
+  console.log(allCars);
+
+  const liAll = document.querySelectorAll('.li');
+
+  liAll.forEach((e, index) => {
+    e.addEventListener('click', () => {
+      currentPage = index + 1;
+      displayPage();
+    });
+  });
 }
